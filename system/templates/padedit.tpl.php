ccf2ad335de3d4745916ab09996f7fef550d5818
d3nj3ll <d3nj3ll@gmail.com>
2012-01-15 18:36:33 +0100

    Changed meta viewport to PREVENT ANY SCALING WHATSOEVER

--- system/templates/padedit.tpl.php
+++ system/templates/padedit.tpl.php
@@ -25,7 +25,7 @@ switch ($action) {
 		<html xmlns="http://www.w3.org/1999/xhtml">
 		<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<meta name = "viewport" content = "user-scalable=no, width=device-width">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 		<title>Welcome to PadEdit</title>
 		<link href="system/styles.css" rel="stylesheet" type="text/css" />
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
