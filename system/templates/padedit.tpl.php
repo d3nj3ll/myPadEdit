833f16269aaa6d5ec754c774996ab3be4c3d645d
d3nj3ll <d3nj3ll@gmail.com>
2012-01-17 16:29:44 +0100

    added global root

--- system/templates/padedit.tpl.php
+++ system/templates/padedit.tpl.php
@@ -17,6 +17,8 @@ if (!defined('PADEDIT_VERSION')){
 	exit;
 }
 
+global $root;
+
 //the following head is used regardless of the action in order to streamline this script.
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
