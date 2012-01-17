65766587380dffe58898508b34d0c7961911b3c1
d3nj3ll <d3nj3ll@gmail.com>
2012-01-17 16:16:32 +0100

    rootrefix

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ session_start();
 if ($_GET['user'] == 'root'){
   // might be nice to force some kind of challenge like password etc.
   global $root;
-    if(empty($_GET)) {
+  //  if(empty($_GET)) {
   $root = '&user=root';
   //  require_once('system/root.php'); //extra features like BASH, moving up the ../../ ladder etc.
 }   
